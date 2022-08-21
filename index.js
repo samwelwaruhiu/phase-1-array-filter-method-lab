eac2d284fb46f61aec5ad0cca0340205bea099e6
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-08-21 16:30:47 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+//const { stringContaining } = require("expect");
+
+function findMatching (drivers,strng){
+    return drivers.filter(drivers => drivers.toUpperCase() === strng.toUpperCase())
+ } 
+
+ function fuzzyMatch (drivers,strng){
+    let querry = strng.length;
+    return drivers.filter(drivers => drivers.slice(0, querry) === strng)
+ }
+ function matchName (driver,strng){
+    return driver.filter(driver => driver.name === strng)
+ } 
+    
+ 
+
